953b762dac4c6c31850221a4c051ca7b3e2c4ff6
Jason <jason10wtsmc@hotmail.com>
2020-08-29 01:44:56 +0800

    Polised

--- product/views.py
+++ product/views.py
@@ -7,12 +7,12 @@ def home(request):
     Product = product.objects
     return render(request, 'product/home.html', {'product': Product})
 
-@login_required
+@login_required(login_url="/account/signup")
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
             Product = product() #induce class product
-            Product.title = request.POST['title']
+            Product.title = request.POST['title'] # object.xxx = input value
             Product.body = request.POST['body']
             if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
                 Product.url = request.POST['url']
@@ -33,7 +33,7 @@ def detail(request, product_id): #urls.py pass request to here
     Product = get_object_or_404(product, pk=product_id)
     return render(request, 'product/detail.html', {'product': Product})
 
-@login_required
+@login_required(login_url="/account/signup")
 def upvote(request, product_id):
     if request.method == 'POST':
         Product = get_object_or_404(product, pk=product_id)
